a3bf44dffbebe1808bbc44788137a196f78c5651
何经纬 <1415997594@qq.com>
2019-02-22 12:23:32 +0800

    用reduce排序。

--- src/spark/example/MyJavaSparkLearning2.java
+++ src/spark/example/MyJavaSparkLearning2.java
@@ -1,23 +1,17 @@
 package spark.example;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.List;
 import java.util.Vector;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.hdfs.web.resources.CreateParentParam;
-import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.spark.SparkConf;
-import org.apache.spark.TaskContext;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
@@ -25,31 +19,10 @@ import org.apache.spark.api.java.function.PairFunction;
 import org.apache.spark.sql.SparkSession;
 
 import parallelSVM.MSSvmTrainer;
+import scala.Enumeration.Val;
 import scala.Tuple2;
 
-public class MyJavaSparkLearning1 {
-
-	// 从本地读取支持向量样本
-	public static Vector<String> ReadTrain(String path) throws IOException {
-		Vector<String> svRecords = new Vector<String>();
-		File file = new File(path);
-
-		BufferedReader br = new BufferedReader(new FileReader(file));
-
-		try {
-			String line;
-			while ((line = br.readLine()) != null && line.length() > 1) {
-				// System.out.println(line);
-				svRecords.addElement(line);
-			}
-		} finally {
-
-			br.close();
-		}
-
-		return svRecords;
-
-	}
+public class MyJavaSparkLearning2 {
 
 	// 从HDFS读取支持向量样本
 	public static Vector<String> ReadTrainFromHDFS(FileSystem fs, Path pt) throws IOException {
@@ -74,10 +47,11 @@ public class MyJavaSparkLearning1 {
 		return svRecords;
 
 	}
-
+	
 	public static void main(String[] args) throws IOException, URISyntaxException {
+		// TODO Auto-generated method stub
 		long startTime=System.currentTimeMillis();
-		SparkConf sparkConf = new SparkConf().setJars(new String[] { "E:\\论文实验\\mjsl1.jar" }).set("spark.executor.memory", "1000m").setMaster("spark://192.168.2.151:7077");
+		SparkConf sparkConf = new SparkConf().set("spark.executor.memory", "4000m").setMaster("local[4]");
 		SparkSession spark = SparkSession.builder().appName("MyJavaSpark").config(sparkConf).getOrCreate();
 		JavaSparkContext jsc = new JavaSparkContext(spark.sparkContext());
 
@@ -85,8 +59,8 @@ public class MyJavaSparkLearning1 {
 		// c,g参数的存储路径
 		String cgPath = "hdfs://192.168.2.151:9000/test/hjw/SvmIn";
 		// c,g参数的数量
-		int cnum = 6;// c
-		int gnum = 6;// g
+		int cnum = 2;// c
+		int gnum = 2;// g
 		// 一次允许运行的Reduce最大数量
 		int maxReduceTask = 16;
 		// 第几次实验
@@ -107,8 +81,6 @@ public class MyJavaSparkLearning1 {
 				out.close();
 			}
 		}
-		// 读取c,g
-		// JavaRDD<String> lines = jsc.textFile("E:\\论文实验\\ModelSelectIn");
 		// 从HDFS读取c,g
 		JavaRDD<String> lines = jsc.textFile(cgPath);
 		System.out.println(lines.collect());
@@ -124,8 +96,6 @@ public class MyJavaSparkLearning1 {
 		JavaPairRDD<String, String> mapToPairLines = lines.mapToPair(new PairFunction<String, String, String>() {
 			public Tuple2<String, String> call(String s) throws Exception {
 
-				// svRecords = ReadTrain("E:\\论文实验\\SVM\\a8t");
-				// svRecords = ReadTrain("E:\\论文实验\\a8trained");
 				System.out.println("开始交叉验证");
 				MSSvmTrainer svmTrainer = new MSSvmTrainer(ssvRecords, Double.parseDouble(s.split("-")[0]),
 						Double.parseDouble(s.split("-")[1]));
@@ -135,9 +105,9 @@ public class MyJavaSparkLearning1 {
 			}
 		});
 
-		// System.out.println("mapToPairLines" + mapToPairLines.collect());
 
-		List<Tuple2<String, String>> list = mapToPairLines.collect();
+
+/*		List<Tuple2<String, String>> list = mapToPairLines.collect();
 		long endTime=System.currentTimeMillis();		
 		System.out.println("网格搜索结束，用时："+(endTime-startTime));
 		
@@ -149,19 +119,24 @@ public class MyJavaSparkLearning1 {
 				max = Double.parseDouble(tuple2._2().replace("%", ""));
 			}
 		}
-		System.out.println("最高准确率："+max+"%");
+		System.out.println("最高准确率："+max+"%");*/
 
-
-		
-		/*
-		 * JavaPairRDD<String, String> mapTest= lines.mapToPair(row->{ return
-		 * new Tuple2(""+TaskContext.getPartitionId(), row); });
-		 * 
-		 * JavaPairRDD<String, String>reduceTest=mapTest.reduceByKey((x,y)->{
-		 * return x+y+"测试Reduce"; //return x+"测试Reduce"; });
-		 * 
-		 * System.out.println("map"+mapTest.collect());
-		 * System.out.println("reduce"+reduceTest.collect());
-		 */
+		//使用sort()排序，没用，只能按照key排序
+		//mapToPairLines.sortByKey();
+		//使用reduce排序
+	Tuple2<String, String> maxACC=	mapToPairLines.reduce((x,y)->{if (Double.parseDouble(x._2().replace("%", ""))>Double.parseDouble(y._2().replace("%", ""))) {
+			return x;
+		}
+		else {
+			return y;
+		}
+		});
+	long endTime=System.currentTimeMillis();		
+	System.out.println("网格搜索结束，用时："+(endTime-startTime));
+	System.out.println("最高准确率："+maxACC);
+	  //删除参数文件
+	  for(int k = 1;k<=count;k++)
+		  fs.delete(new Path(cgPath +"/"  + String.valueOf(k)),true);		
 	}
+
 }
